5f351d90a652d55adfa6002d71275c57fa19da62
Anandha Ragaven <ragavenr1301@gmail.com>
2026-07-07 23:41:09 -0400

    feat(audio): implement microphone audio capture

---
+++ src/audio_capture.py
@@ -0,0 +1,25 @@
+import sounddevice as sd
+from scipy.io.wavfile import write
+
+
+SAMPLE_RATE = 16000
+DURATION = 5
+OUTPUT_FILE = "recoding.wav";
+
+def record_audio():
+    print("Recoding... ")
+    audio = sd.rec(
+        int(DURATION * SAMPLE_RATE),
+        samplerate=SAMPLE_RATE,
+        channels=1,
+        dtype="float32"
+
+    ) # NumPy Array (80000 × 1)
+    sd.wait()
+
+    write(OUTPUT_FILE,SAMPLE_RATE,audio)
+    print(f"Saved to {OUTPUT_FILE}")
+
+
+if __name__ == "__main__":
+    record_audio()
